f6134822b325336a5a6ff8803c4509b4c3137250
Kia Panahi Rad <kia.panahirad@gmail.com>
2020-11-23 20:07:24 +0330

    Add contracts

---
+++ src/Paxos.Core/Contracts/IAcceptor.cs
@@ -0,0 +1,10 @@
+﻿using System.Threading.Tasks;
+
+namespace Paxos.Core.Contracts
+{
+    public interface IAcceptor
+    {
+        Task<PrepareResponse> ReceivePrepareRequest(PrepareRequest request);
+        Task ReceiveAcceptRequest<T>(AcceptRequest<T> request) where T : Proposal<T>;
+    }
+}
---
+++ src/Paxos.Core/Contracts/IProposer.cs
@@ -0,0 +1,10 @@
+﻿using System.Threading.Tasks;
+
+namespace Paxos.Core.Contracts
+{
+    public interface IProposer<T>
+    {
+        Task<PrepareResponse> SendPrepareRequestAsync();
+        Task SendAcceptRequest(AcceptRequest<T> request);
+    }
+}
