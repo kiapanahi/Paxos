72d21905ff6f5a5b1fdfbc196be52d1b32ecdd99
Kia Panahi Rad <kia.panahirad@gmail.com>
2020-11-23 20:07:18 +0330

    Add models

---
+++ src/Paxos.Core/Models.cs
@@ -0,0 +1,9 @@
+﻿namespace Paxos.Core
+{
+    public record Proposal(long Number);
+    public record Proposal<TValue>(long Number, TValue Value) : Proposal(Number);
+    public record PrepareRequest(long Number);
+    public record PrepareResponse(bool Promised, Proposal? AcceptedProposal);
+    public record AcceptRequest<TValue>(Proposal<TValue> Proposal);
+    public record AcceptResponse(bool Accepted);
+}
