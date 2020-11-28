6d5910dc9d7ab65ffc2a7bbcc4636cf5f55a54d1
Kia Panahi Rad <kia.panahirad@gmail.com>
2020-11-29 00:37:48 +0330

    Refactor EpochProposerNumberGenerator to its own file

--- src/Paxos.Core/Contracts/IProposerNumberGenerator.cs
+++ src/Paxos.Core/Contracts/IProposerNumberGenerator.cs
@@ -1,16 +1,8 @@
-﻿using System;
-
-namespace Paxos.Core.Contracts
+﻿namespace Paxos.Core.Contracts
 {
     public interface IProposerNumberGenerator
     {
         long Next();
         long Next(long minValue);
-    }
-
-    public class EpochProposerNumberGenerator : IProposerNumberGenerator
-    {
-        public long Next() => new DateTimeOffset().ToUnixTimeMilliseconds();
-        public long Next(long minValue) => Next() + minValue;
     }
 }
---
+++ src/Paxos.Core/EpochProposerNumberGenerator.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using Paxos.Core.Contracts;
+
+namespace Paxos.Core
+{
+    public class EpochProposerNumberGenerator : IProposerNumberGenerator
+    {
+        public long Next() => new DateTimeOffset().ToUnixTimeMilliseconds();
+        public long Next(long minValue) => Next() + minValue;
+    }
+}
