17eec0de6d0e292c5073b239c21ae63b93ddcdf1
Kia Panahi Rad <kia.panahirad@gmail.com>
2020-11-29 15:11:48 +0330

    remove comment

--- src/Paxos.Core/Models.cs
+++ src/Paxos.Core/Models.cs
@@ -4,9 +4,9 @@
     {
         public T? As<T>() where T: class => this as T;
     }
+
     public record Proposal<TValue>(long Number, TValue Value) : Proposal(Number);
 
-    //public record PrepareRequest<TValue>(Proposal<TValue> Proposal);
     public record PrepareRequest(Proposal Proposal);
     public record PrepareResponse(bool Promised, Proposal? AcceptedProposal);
     
